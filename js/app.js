eab9ecffb30a1a7af42b6ea9a86060ccab4fa066
MichaelWrock <mike.wrock@seekmomentum.com>
2016-01-05 15:04:41 -0500

    Update Scroll Method
    Re-Evaluate Speed to Ensure a both a Smoother and Faster scroll across Viewports

--- js/app.js
+++ js/app.js
@@ -42,26 +42,25 @@ documentEl.on('scroll', function(){
 
 var scrollY = 0;
 var distance = 40;
-var speed = 24;
+var speedMultiplier = 6;
 function autoScrollTo(el) {
 	var currentY = window.pageYOffset;
 	var targetY = document.getElementById(el).offsetTop;
 	var bodyHeight = document.body.offsetHeight;
 	var yPos = currentY + window.innerHeight;
-	var animator = setTimeout('autoScrollTo(\''+el+'\')',24);
+
+	var speed = ((targetY - currentY) / targetY) * speedMultiplier;
+	
+	var animator = setTimeout('autoScrollTo(\''+el+'\')',speed);
 	if(yPos > bodyHeight){
 		clearTimeout(animator);
 	} else {
 		if(currentY < targetY-distance){
 		    scrollY = currentY+distance;
 		    window.scroll(0, scrollY);
+			console.log(speed)
 	    } else {
 		    clearTimeout(animator);
 	    }
 	}
 }
-
-
-
-
-
